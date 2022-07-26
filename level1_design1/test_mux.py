58c5386833d70ff5e66a49bbccb040122cde4d80
Thalp <t.thalpasai@gmail.com>
2022-07-26 13:44:44 +0000

    updated testfile

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,36 +6,40 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
- 5'b00000: out = inp0
- 5'b00001: out = inp1  
-      5'b00010: out = inp2;  
-      5'b00011: out = inp3;  
-      5'b00100: out = inp4;  
-      5'b00101: out = inp5;  
-      5'b00110: out = inp6;  
-      5'b00111: out = inp7;  
-      5'b01000: out = inp8;  
-      5'b01001: out = inp9;  
-      5'b01010: out = inp10;
-      5'b01011: out = inp11;
-      5'b01101: out = inp12;
-      5'b01101: out = inp13;
-      5'b01110: out = inp14;
-      5'b01111: out = inp15;
-      5'b10000: out = inp16;
-      5'b10001: out = inp17;
-      5'b10010: out = inp18;
-      5'b10011: out = inp19;
-      5'b10100: out = inp20;
-      5'b10101: out = inp21;
-      5'b10110: out = inp22;
-      5'b10111: out = inp23;
-      5'b11000: out = inp24;
-      5'b11001: out = inp25;
-      5'b11010: out = inp26;
-      5'b11011: out = inp27;
-      5'b11100: out = inp28;
-      5'b11101: out = inp29;
+ begin 
+ case(inp,sel)
+ inp0=5'b00000; sel=5'b00000;
+ inp1=5'b00001; sel=5'b00001;
+ inp2=5'b00010; sel=5'b00010;
+ inp3=5'b00011; sel=5'b00011;
+ inp4=5'b00100; sel=5'b00100;
+ inp5=5'b00101; sel=5'b00101;
+ inp6=5'b00110; sel=5'b00110;
+ inp7=5'b00111; sel=5'b00111;
+ inp8=5'b01000; sel=5'b01000;
+ inp19=5'b01001; sel=5'b01001;
+ inp10=5'b01010; sel=5'b01010;
+ inp11=5'b01011; sel=5'b01011;
+ inp12=5'b01100; sel=5'b01100;
+ inp13=5'b01101; sel=5'b01101;
+ inp14=5'b01110; sel=5'b01110;
+ inp15=5'b01111; sel=5'b01111;
+ inp16=5'b10000; sel=5'b10000;
+ inp17=5'b10001; sel=5'b10001;
+ inp18=5'b10010; sel=5'b00010;
+ inp19=5'b10011; sel=5'b10011;
+ inp20=5'b10100; sel=5'b10100;
+ inp21=5'b10101; sel=5'b10101;
+ inp22=5'b10110; sel=5'b10110;
+ inp23=5'b10111; sel=5'b10111;
+ inp24=5'b11000; sel=5'b11000;
+ inp25=5'b11001; sel=5'b11001;
+ inp26=5'b11010; sel=5'b11011;
+ inp27=5'b11011; sel=5'b11011;
+ inp28=5'b11100; sel=5'b11100;
+ inp29=5'b11101; sel=5'b11101;
+ inp30=5'b11110; sel=5'b11110;
+ inp31=5'b11111; sel=5'b11111;
        #input driving
 dut.inp0.value=sel0
 dut.inp1.value=sel1
