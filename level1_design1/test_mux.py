a08a4c4d093053da0cd4bf6046bda539405a345e
Thalp <t.thalpasai@gmail.com>
2022-07-26 13:56:46 +0000

    update testfile

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -6,40 +6,39 @@ import random
 @cocotb.test()
 async def test_mux(dut):
     """Test for mux2"""
- begin 
- case(inp,sel)
- inp0=5'b00000; sel=5'b00000;
- inp1=5'b00001; sel=5'b00001;
- inp2=5'b00010; sel=5'b00010;
- inp3=5'b00011; sel=5'b00011;
- inp4=5'b00100; sel=5'b00100;
- inp5=5'b00101; sel=5'b00101;
- inp6=5'b00110; sel=5'b00110;
- inp7=5'b00111; sel=5'b00111;
- inp8=5'b01000; sel=5'b01000;
- inp19=5'b01001; sel=5'b01001;
- inp10=5'b01010; sel=5'b01010;
- inp11=5'b01011; sel=5'b01011;
- inp12=5'b01100; sel=5'b01100;
- inp13=5'b01101; sel=5'b01101;
- inp14=5'b01110; sel=5'b01110;
- inp15=5'b01111; sel=5'b01111;
- inp16=5'b10000; sel=5'b10000;
- inp17=5'b10001; sel=5'b10001;
- inp18=5'b10010; sel=5'b00010;
- inp19=5'b10011; sel=5'b10011;
- inp20=5'b10100; sel=5'b10100;
- inp21=5'b10101; sel=5'b10101;
- inp22=5'b10110; sel=5'b10110;
- inp23=5'b10111; sel=5'b10111;
- inp24=5'b11000; sel=5'b11000;
- inp25=5'b11001; sel=5'b11001;
- inp26=5'b11010; sel=5'b11011;
- inp27=5'b11011; sel=5'b11011;
- inp28=5'b11100; sel=5'b11100;
- inp29=5'b11101; sel=5'b11101;
- inp30=5'b11110; sel=5'b11110;
- inp31=5'b11111; sel=5'b11111;
+ begin
+ sel0=1
+ sel1=1
+ sel2=1
+ sel3=1
+ sel4=1
+ sel5=1
+ sel6=1
+ sel7=0
+ sel8=1
+ sel9=1
+ sel10=1
+ sel11=1
+ sel12=1
+ sel13=0
+ sel14=1
+ sel15=1
+ sel16=1
+ sel17=1
+ sel18=1
+ sel19=0 
+ sel20=0
+ sel21=1
+ sel22=1
+ sel23=0
+ sel24=1
+ sel25=0
+ sel26=1 
+ sel27=1
+ sel28=0
+ sel29=1
+ sel30=0
+ sel31=1
        #input driving
 dut.inp0.value=sel0
 dut.inp1.value=sel1
