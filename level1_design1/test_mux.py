b77d21e5af3b4a7e7229c19183e02c5a1a9ba09c
Thalp <t.thalpasai@gmail.com>
2022-07-29 05:02:40 +0000

    Update test file

--- level1_design1/test_mux.py
+++ level1_design1/test_mux.py
@@ -40,11 +40,7 @@ dut.inp28.value=28;
 dut.inp29.value=29;
 dut.inp30.value=30;
 dut.inp31.value=31;
-
-
-
-await Timer(2,units='ns')
-
+return await Timer
 assert dut.sel.value==sel,f"sel result is incorrect:{dut.sel.value}!=5'b11111"
 
 
