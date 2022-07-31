868e2579ab463f9156526c9c19e78157de16a0cd
Thalp <t.thalpasai@gmail.com>
2022-07-31 06:03:13 +0000

    Updated test file

--- level3_design/test_encoder.py
+++ level3_design/test_encoder.py
@@ -9,11 +9,25 @@ async def test_encoder(dut):
     """Test for encoder"""
 
     cocotb.log.info('#### CTB: Develop your test here ####')
-    enc_y6=1,
-    enc_y5=1,
-    enc_y4=1,
-    enc_y3,
-    enc_y2,
-    enc_y1,
-    enc_y0,
-    EN_enc,
+    enc_decla_y7=0,
+    enc_decla_y6=1,
+    enc_decla_y5=1,
+    enc_decla_y4=1,
+    enc_decla_y3=0,
+    enc_decla_y2=1,
+    enc_decla_y1=1,
+    enc_decla_y0=1,
+    
+    #input driving
+    dut.enc_y7.value=enc_decla_y7
+    dut.enc_y6.value=enc_decla_y6
+    dut.enc_y5.value=enc_decla_y5
+    dut.enc_y4.value=enc_decla_y4
+    dut.enc_y3.value=enc_decla_y3
+    dut.enc_y2.value=enc_decla_y2
+    dut.enc_y1.value=enc_decla_y1
+    dut.enc_y0.value=enc_decla_y0
+
+    await Timer(1,units='ns')
+
+    assert dut.RDY_out.value==0,f"encoder result is incorrect:{dut.RDY_out.value!=1}"
