d9f8bd4e05c3483b7c3550931f11e0d6df0b0cdd
Thalp <t.thalpasai@gmail.com>
2022-08-01 10:38:20 +0000

    Updated Testfile

--- level3_design/encoder.v
+++ level3_design/encoder.v
@@ -4,21 +4,21 @@
 //
 // Ports
 // Name            I/O size
-// enc_y7value      8bits
-// enc_y6value      8bits
-// enc_y5value      8bits
-// enc_y4value      8bits
-// enc_y3value      8bits
-// enc_y2value      8bits
-// enc_y1value      8bits
-// enc_y0value      8bits
+// enc_y7value      16bits
+// enc_y6value      16bits
+// enc_y5value      16bits
+// enc_y4value      16bits
+// enc_y3value      16bits
+// enc_y2value      16bits
+// enc_y1value      16bits
+// enc_y0value      16bits
+// out_a3value      1bits
 // out_a2value      1bits
 // out_a1value      1bits
 // out_a0value      1bits
-// out RDY_out      3bits
 //
 // encoded information signal from input to output ports
-// (enc_y7, enc_y6,enc_y5, enc_y4,enc_y3,enc_y2,enc_y1,enc_y0) ->out_a0value,out_a1value,out_a2value
+// (enc_y7, enc_y6,enc_y5, enc_y4,enc_y3,enc_y2,enc_y1,enc_y0) ->out_a0value,out_a1value,out_a2value,out_a3value
 // input Ports ,output ports declaration 
 module encoder(enc_y7,
      enc_y6,
@@ -28,6 +28,7 @@ module encoder(enc_y7,
      enc_y2,
      enc_y1,
      enc_y0,
+     out_a3value,
      out_a2value,
      out_a1value,
      out_a0value);
@@ -39,9 +40,11 @@ input [7:0] enc_y3;
 input [7:0] enc_y2;
 input [7:0] enc_y1;
 input [7:0] enc_y0;
+output out_a3value;
 output out_a2value;
 output out_a1value;
 output out_a0value;
+reg out_a3value;
 reg out_a2value;
 reg out_a1value;
 reg out_a0value;
@@ -60,6 +63,7 @@ reg out_a0value;
   assign enc_y0=8'b00000001;
  // scheduling output signals
  //
+ assign out_a3value ={1};
  assign out_a2value = { enc_y7|enc_y6|enc_y5|enc_y4};
  assign out_a1value = { enc_y7|enc_y6|enc_y3|enc_y2};
  assign out_a0value = { enc_y7|enc_y5|enc_y3|enc_y1};
