0c8640f6367599400d15b45a9881c17b329ffae7
Thalp <t.thalpasai@gmail.com>
2022-08-01 10:25:17 +0000

    Updated Testfile

--- level1_design2/level1_design2_buggy/test_seq_detect_1011.py
+++ level1_design2/level1_design2_buggy/test_seq_detect_1011.py
@@ -30,7 +30,7 @@ async def test_seq_bug1(dut):
     await FallingEdge(dut.clk)
     dut.inp_bit.value=1
     await FallingEdge(dut.clk)
-    dut.inp_bit.value=1
+    dut.inp_bit.value=0
     await FallingEdge(dut.clk)
     
 
